87d7b38e10a70c4c1b6dd007457acd624d888863
Manuel Konrath <manuel.konrath@gmail.com>
2018-02-20 00:32:04 +0100

    Enlève le include head dans le game.php
    Il y a le link vers le css MainProfile, qu'on ne veut pas dedans ...

--- game.php
+++ game.php
@@ -1,7 +1,25 @@
 <!doctype html>
 <html lang="fr">
 
-<?include 'head.php' ?>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- CSS profile -->
+<link rel="stylesheet" type="text/css" href="../CSS/GameProfile.css">
+
+    <!-- Police Google fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Sedgwick+Ave+Display" rel="stylesheet">
+
+
+
+    <title>Morpion Ninja</title>
+  </head>
 <link rel="stylesheet" type="text/css" href="../CSS/GameProfile.css">
 
     <!-- Notre JavaScript -->
