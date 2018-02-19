495e93d0d1fff0f855853779899247ed0df72847
Manuel Konrath <manuel.konrath@gmail.com>
2018-02-20 00:28:43 +0100

    Test
    AAhhh on a perdu quelques fonctionnalité là ...

--- game.php
+++ game.php
@@ -2,6 +2,10 @@
 <html lang="fr">
 
 <?include 'head.php' ?>
+<link rel="stylesheet" type="text/css" href="../CSS/GameProfile.css">
+
+    <!-- Notre JavaScript -->
+    <script type="text/javascript" src="js/script.js"></script>
 
   <body>
     <h1>Welcome to the game</h1>
@@ -30,8 +34,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-    <!-- Notre JavaScript -->
-    <script type="text/javascript" src="..js/script.js"></script>
+
 
   </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -1,55 +1,55 @@
+
 // récupérer le nom des joueurs 
-var player1 =prompt('joeur 1 entrez votre nom'); 
-var player2 =prompt('joeur 2 entrez votre nom');
+var player1 =prompt('Joueur 1 entrez votre nom'); 
+var player2 =prompt('Joueur 2 entrez votre nom');
+var nbrClic = 0, player;
 
-
-    	var nbrClic = 0, player;
-       
-
-    
-
-
-    function annonce (nbrClic){
-
-        if (nbrClic==0){
-        player=player1;
-        var annonce = document.getElementById('annonce');
-        var textannonce = document.createTextNode('c\'est à '+player+' de jouer !');
-        annonce.appendChild(textannonce);
-    } else if (nbrClic%2==0) {
-        player=player1;
-        var annonce = document.getElementById('annonce');
-        var textannonce = document.createTextNode('c\'est à '+player+' de jouer !');
-        annonce.replaceChild(textannonce);
-    } 
-}
-
-annonce();
+//Tableau de valeur, joueur 1 : X, joueur 2 : O 
+Morpion = new Array(9);
+        
+        //cette fonction annonce le nom du joueur à chaques tours
+        function annonce (nbrClic, player){
+            if (nbrClic%2==0) {
+                player=player1;
+                document.getElementById('annonce').innerHTML = "C\'est à "+ player + " de jouer";
+            }
+            else {
+                player=player2;
+                document.getElementById('annonce').innerHTML = "C\'est à "+ player + " de jouer";
+            }
+        }
 
 
 
-    	function myFunction (id, nbrClic) {
-    		
-            if(nbrClic<=8) {
-    			if(nbrClic%2==0){
-					id.style.backgroundColor= "red";    
-    			} 
-    			else {
-    				id.style.backgroundColor= "blue";   
-    			}
-    		}
-    		else {
-    			alert ("La partie est terminée")
-    		}
-           
-		} 
+        //Cette fonction teste l'alignement des valeur X et O dans le tableau Morpion
+        function testAlignement(Morpion, player1, player2) {
+            if(Morpion[0]+Morpion[1]+Morpion[2]=="XXX"||Morpion[3]+Morpion[4]+Morpion[5]=="XXX"||Morpion[6]+Morpion[7]+Morpion[8]=="XXX"||Morpion[0]+Morpion[3]+Morpion[6]=="XXX"||Morpion[1]+Morpion[4]+Morpion[7]=="XXX"||Morpion[2]+Morpion[5]+Morpion[8]=="XXX"||Morpion[0]+Morpion[4]+Morpion[8]=="XXX"||Morpion[2]+Morpion[4]+Morpion[6]=="XXX") {
+                alert(player1 + ' gagne la partie');
+                window.location.reload();
+            }
+            else if (Morpion[0]+Morpion[1]+Morpion[2]=="OOO"||Morpion[3]+Morpion[4]+Morpion[5]=="OOO"||Morpion[6]+Morpion[7]+Morpion[8]=="OOO"||Morpion[0]+Morpion[3]+Morpion[6]=="OOO"||Morpion[1]+Morpion[4]+Morpion[7]=="OOO"||Morpion[2]+Morpion[5]+Morpion[8]=="OOO"||Morpion[0]+Morpion[4]+Morpion[8]=="OOO"||Morpion[2]+Morpion[4]+Morpion[6]=="OOO") {
+                alert(player2 + ' gagne la partie');
+                window.location.reload();
+            }
+            else {
+            }
+        }
 
+        //Cette fonction change la couleur de fond des cases en fonction du nbr de clics
+        function myFunction (id, nbrClic, caseid) {
+            if(nbrClic<=8) { 
+                if(nbrClic%2==0){
+                    id.style.backgroundColor= "red";
+                    Morpion[caseid]= "X";
+                } 
+                else {
+                    id.style.backgroundColor= "blue";
+                    Morpion[caseid]= "O";
+                }
+            }
+            else {
+                alert ("La partie est terminée")
+                window.location.reload();
+            }
+        } 
 
-
-
-
-/*function annonceplayer () {
-    var annonce = document.getElementById('annonce');
-    var textannonce = document.createTextNode('c\'est à '+player+' de jouer !');
-    annonce.appendChild(textannonce);
-} */
