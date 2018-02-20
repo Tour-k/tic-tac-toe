2628a17b461aa13766041a6c4d08018ed990edd9
Manuel Konrath <manuel.konrath@gmail.com>
2018-02-21 00:18:07 +0100

    test css

--- game.php
+++ game.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
     <!-- CSS profile -->
-	<link rel="stylesheet" type="text/css" href="../CSS/GameProfile.css">
+	<link rel="stylesheet" type="text/css" href="CSS/GameProfile.css">
 
     <!-- Police Google fonts -->
     <link href="https://fonts.googleapis.com/css?family=Sedgwick+Ave+Display" rel="stylesheet">
