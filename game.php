bb4c18e341af603f8416c9a851c31ea4bda05f19
Manuel Konrath <manuel.konrath@gmail.com>
2018-02-20 23:03:49 +0100

    Revert "Merge branch 'Manuel_02' into Manuel"
    This reverts commit 98323e7c1c12a09ddda4bc8516d805460bd86507, reversing
    changes made to 0db697f314ecf509e3b1ad21e1077f7357c226d6.

--- game.php
+++ game.php
@@ -16,7 +16,6 @@
     <!-- Police Google fonts -->
     <link href="https://fonts.googleapis.com/css?family=Sedgwick+Ave+Display" rel="stylesheet">
 
-   
 
     <title>Morpion Ninja - Start the game</title>
   </head>
@@ -25,38 +24,34 @@
     	<div id="annonce"></div>
 		<table onclick="nbrClic++; testAlignement(Morpion, player1, player2); annonce2(nbrClic, player);">
 			<tr>
-				<td id="case0" onclick= "myFunction(this, nbrClic, 0);"></td>
-				<td id="case1" onclick= "myFunction(this, nbrClic, 1);"></td>
-				<td id="case2" onclick= "myFunction(this, nbrClic, 2);"></td>
+				<td id="case0" onclick= "changeColorBackGround(this, nbrClic, 0);"></td>
+				<td id="case1" onclick= "changeColorBackGround(this, nbrClic, 1);"></td>
+				<td id="case2" onclick= "changeColorBackGround(this, nbrClic, 2);"></td>
 			</tr>
 			<tr>
-				<td id="case3" onclick= "myFunction(this, nbrClic, 3);"></td>
-				<td id="case4" onclick= "myFunction(this, nbrClic, 4);"></td>
-				<td id="case5" onclick= "myFunction(this, nbrClic, 5);"></td>
+				<td id="case3" onclick= "changeColorBackGround(this, nbrClic, 3);"></td>
+				<td id="case4" onclick= "changeColorBackGround(this, nbrClic, 4);"></td>
+				<td id="case5" onclick= "changeColorBackGround(this, nbrClic, 5);"></td>
 			</tr>
 			<tr>
-				<td id="case6" onclick= "myFunction(this, nbrClic, 6);"></td>
-				<td id="case7" onclick= "myFunction(this, nbrClic, 7);"></td>
-				<td id="case8" onclick= "myFunction(this, nbrClic, 8);"></td>
+				<td id="case6" onclick= "changeColorBackGround(this, nbrClic, 6);"></td>
+				<td id="case7" onclick= "changeColorBackGround(this, nbrClic, 7);"></td>
+				<td id="case8" onclick= "changeColorBackGround(this, nbrClic, 8);"></td>
 			</tr>
 		</table>
 
 
-        
+    <!-- Notre JavaScript -->
+    <script type="text/javascript" src="js/script.js"></script>
+
+
     <!-- Optional JavaScript -->
- <!-- Notre JavaScript -->
-
-    <script type="text/javascript" src="js/script.js">
-/*il vaut mieux mettre le javascript en fin de page html, juste avant la fermeture de body. 
-Comme ça la page html peut se charger avant de charger le javascript. */
-    </script>
-
-
-
     <!-- jQuery first, then Popper.js, then Bootstrap JS-->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
 
   </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -7,34 +7,33 @@ var nbrClic = 0, player;
 //Tableau de valeur, joueur 1 : X, joueur 2 : O 
 Morpion = new Array(9);
         
-//ces fonctions annoncent le nom du joueur à chaques tours
-function annonce1 () {
-    var textannonce1=document.createTextNode(player1 +' doit jouer');
-    annonce.appendChild(textannonce1);
-}
-        
-    annonce1();
+        //cette fonction annonce le nom du joueur à chaques tours
+        function annonce1 () {
+            var textannonce1=document.createTextNode(player1 +' doit jouer');
+            annonce.appendChild(textannonce1);
+        }
+        annonce1();
 
-function annonce2 (nbrClic) { 
-
-    var textannonce3=document.createTextNode(player1 +' doit jouer');
-    var textannonce2=document.createTextNode(player2 +' doit jouer');
-
-    if (nbrClic%2==0){
-        var annonce=document.querySelector('#annonce');
-        var remove1=annonce.lastChild;
-        annonce.removeChild(remove1);
-        annonce.appendChild(textannonce3); 
-    } else if(nbrClic%2!==0){
-        var annonce=document.querySelector('#annonce');
-        var remove1=annonce.lastChild;
-        annonce.removeChild(remove1);
-        annonce.appendChild(textannonce2);  
-    }
-}
+        function annonce2 (nbrClic) { 
+            var textannonce3=document.createTextNode(player1 +' doit jouer');
+            var textannonce2=document.createTextNode(player2 +' doit jouer');
 
 
-//Cette fonction teste l'alignement des valeur X et O dans le tableau Morpion
+            if (nbrClic%2==0){
+                var annonce=document.querySelector('#annonce');
+                var remove1=annonce.lastChild;
+                annonce.removeChild(remove1);
+                annonce.appendChild(textannonce3); 
+            } else {
+                var annonce=document.querySelector('#annonce');
+                var remove1=annonce.lastChild;
+                annonce.removeChild(remove1);
+                annonce.appendChild(textannonce2);  
+            }
+        }
+
+
+        //Cette fonction teste l'alignement des valeur X et O dans le tableau Morpion
         function testAlignement(Morpion, player1, player2) {
             if(Morpion[0]+Morpion[1]+Morpion[2]=="XXX"||Morpion[3]+Morpion[4]+Morpion[5]=="XXX"||Morpion[6]+Morpion[7]+Morpion[8]=="XXX"||Morpion[0]+Morpion[3]+Morpion[6]=="XXX"||Morpion[1]+Morpion[4]+Morpion[7]=="XXX"||Morpion[2]+Morpion[5]+Morpion[8]=="XXX"||Morpion[0]+Morpion[4]+Morpion[8]=="XXX"||Morpion[2]+Morpion[4]+Morpion[6]=="XXX") {
                 alert(player1 + ' gagne la partie');
@@ -48,10 +47,8 @@ function annonce2 (nbrClic) {
             }
         }
 
-
-//Cette fonction change la couleur de fond des cases en fonction du nbr de clics
-        function myFunction (id, nbrClic, caseid) {
-
+        //Cette fonction change la couleur de fond des cases en fonction du nbr de clics
+        function changeColorBackGround (id, nbrClic, caseid) {
             if(nbrClic<=8) { 
                 if(nbrClic%2==0){
                     id.style.backgroundColor= "red";
